86a64f53f5572fd868120d30962f55aa66507734
Ahmet Karakoc <go72nux@mytum.de>
2026-07-08 12:46:44 +0200

    finished annualized return calculation without cost metrics focusing on the market asset based calculations

--- analytics/performance.py
+++ analytics/performance.py
@@ -9,11 +9,19 @@ class PerformanceCalculator():
         self.trading_days = 252
 
     def total_return(self):
-        
+
         growth_factors = self.portfolio_returns + 1
         total_return = growth_factors.prod() - 1 
 
         return total_return
+    
+    def annualized_return(self):
+        total_days = len(self.portfolio_returns)
+        growth_factors = self.portfolio_returns + 1
+        total_ret = growth_factors.prod() - 1 
+        ann_return = (1 + total_ret)**(self.trading_days/total_days) - 1
+
+        return ann_return
 
     
 
--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from domain.transaction import Transaction
 from domain.position import Position
 from domain.portfolio import Portfolio
 from data.market_data import MarketDataFetcher
-import pandas as pd
-import yfinance as yf
 from analytics.timeseries import PortfolioTimeSeries
 from analytics.performance import PerformanceCalculator
 
 if __name__ == "__main__":
 
     # 1. Create your transactions
-    transaction1 = Transaction('aapl', '2024-01-01','BUY', 5, 100, 1,'USD')
-    transaction2 = Transaction('msft', '2024-01-15','BUY', 5, 120, 1,'USD')
-    transaction3 = Transaction('tsla', '2024-01-18','BUY', 2, 10 , 1,'USD')
-    transaction4 = Transaction('nvda', '2024-01-18','BUY', 2, 200 , 1,'USD')
+    transaction1 = Transaction('aapl', '2024-01-01','BUY', 1, 120, 2,'USD')
+
 
     # 2. Create your portfolio
     port1 = Portfolio("ATK_1", "USD", creation_date = '2024-01-01')
@@ -21,13 +17,11 @@ if __name__ == "__main__":
     # 3. Add transactions directly to the portfolio. 
     # Your Portfolio class will automatically group them into an 'aapl' Position!
     port1.add_transaction(transaction1)
-    port1.add_transaction(transaction2)
-    port1.add_transaction(transaction3)
-    port1.add_transaction(transaction4)
-    
+
+
     # 4. Run your timeseries
     fetcher = MarketDataFetcher()
-    time_s = PortfolioTimeSeries(port1, fetcher=fetcher, start_date="2024-01-01", end_date="2025-01-01")
+    time_s = PortfolioTimeSeries(port1, fetcher=fetcher, start_date="2024-01-01")
     
     a = time_s.portfolio_value()
     b = time_s.portfolio_returns()
